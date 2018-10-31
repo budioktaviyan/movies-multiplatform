a72e339daa331f1ce86d6eb24e26a78fe2911349
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-10-31 12:41:22 +0700

    Cleanup dependencies

--- common-client-jvm/src/main/kotlin/id/kotlin/multiplatform/Dispatcher.kt
+++ common-client-jvm/src/main/kotlin/id/kotlin/multiplatform/Dispatcher.kt
@@ -1,7 +1,1 @@
 package id.kotlin.multiplatform
-
-import kotlinx.coroutines.experimental.CoroutineDispatcher
-import kotlinx.coroutines.experimental.Dispatchers
-
-internal actual val ApplicationDispatcher: CoroutineDispatcher = Dispatchers.Default
-internal actual val UIDispatcher: CoroutineDispatcher = Dispatchers.Main
--- common-client/src/main/kotlin/id/kotlin/multiplatform/Dispatcher.kt
+++ common-client/src/main/kotlin/id/kotlin/multiplatform/Dispatcher.kt
@@ -1,6 +1,1 @@
 package id.kotlin.multiplatform
-
-import kotlinx.coroutines.experimental.CoroutineDispatcher
-
-internal expect val ApplicationDispatcher: CoroutineDispatcher
-internal expect val UIDispatcher: CoroutineDispatcher
