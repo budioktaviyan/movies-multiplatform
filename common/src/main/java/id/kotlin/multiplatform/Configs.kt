540bb88b47f1fe720a315db3c7a85a31dd477c5c
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-11-02 01:26:08 +0700

    Fix issue on building native framework

--- common/src/main/java/id/kotlin/multiplatform/Configs.kt
+++ common/src/main/java/id/kotlin/multiplatform/Configs.kt
@@ -2,7 +2,5 @@ package id.kotlin.multiplatform
 
 object Configs {
 
-    const val API_KEY = "f920accbb779fcb3ab3bbec9a8b40bd0"
-    const val BASE_URL = "https://api.themoviedb.org/3"
     const val IMAGE_URL = "https://image.tmdb.org/t/p/original"
 }
