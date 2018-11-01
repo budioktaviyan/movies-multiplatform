9c3ddb76186d5263bb8051fbfbd445df163ea0d0
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-11-01 08:06:23 +0700

    Initial ios project

---
+++ ios/Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Budi Oktaviyan on 01/11/18.
+//  Copyright © 2018 Kotlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
